fbc7e4086a947cb0b31d0a16a55ab40c90359b0b
akirsteina <akirsteina@users.noreply.github.com>
2021-07-17 20:05:07 +0300

    add page404 content

--- front-end-react/src/Views/Page404.js
+++ front-end-react/src/Views/Page404.js
@@ -1,8 +1,18 @@
+
+import {NavLink} from 'react-router-dom';
+
 function Page404() {
 
     return (
-        <div>
-            This is Page 404
+        <div className="container page404 d-flex justify-content-center flex-column">
+            <div className="row py-5 align-content-center">
+                <div className="col-6 offset-3 text-center">
+                    <h1>Whoops...</h1>
+                    <p className="lead">Looks like something went wrong!</p>
+                    <NavLink className="btn btn-primary mx-2" to="/" role="button">Go back home</NavLink>
+                    <NavLink className="btn border-danger mx-2" to="/" role="button">Report an error</NavLink>
+                </div>
+            </div>
         </div>
     )
 }
