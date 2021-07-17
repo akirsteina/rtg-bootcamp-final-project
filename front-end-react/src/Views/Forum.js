407d11cab07a6e9d7bd83d35eea215d6f6986b9d
akirsteina <akirsteina@users.noreply.github.com>
2021-07-17 14:23:23 +0300

    create forum content

--- front-end-react/src/Views/Forum.js
+++ front-end-react/src/Views/Forum.js
@@ -1,9 +1,30 @@
+
+import Breadcrumbs from '../Components/Breadcrumbs';
+import '../Assets/Css/forum.css';
+import ChatMessagesList from '../Components/ChatMessagesList';
+
 function Forum() {
+    const breadcrumbPaths = [
+        { link: '/', label: 'Home' },
+        { label: 'Forum' }
+    ];
 
     return (
-        <div>
-            This is Forum
-        </div>
+        <main>
+            <div className="container">
+                <div className="row pt-5 mt-5">
+                    <div className="col-12 breadcrumbs">
+                        <Breadcrumbs paths={breadcrumbPaths} />
+                    </div>
+                </div>
+                <div className="row my-5 py-3">
+                    <div className="col-12 chat-window rounded">
+                        <h3 className="special-title main-header py-3 fs-2">Happy plants forum</h3>
+                        <ChatMessagesList/>
+                    </div>
+                </div>
+            </div>
+        </main >
     )
 }
 
