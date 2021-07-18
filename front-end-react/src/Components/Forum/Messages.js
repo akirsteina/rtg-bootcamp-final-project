22772d7a6bfc950d0b2985e243db1b4a8f678f08
akirsteina <akirsteina@users.noreply.github.com>
2021-07-18 15:03:27 +0300

    clean up code

--- front-end-react/src/Components/Forum/Messages.js
+++ front-end-react/src/Components/Forum/Messages.js
@@ -2,26 +2,29 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
-function Messages({messages}) {
+function Messages({ messages }) {
 
     if (!messages.loading && messages.length === 0) {
         return <h5> Something went wrong :( </h5>;
     }
-     else if (!messages.loading) {
+    else if (!messages.loading) {
+
+
         return ([...messages.items].reverse().map((message, index) => {
             return (
                 <li key={index} className="list-group-item">
-                <div className="row">
-                    <div className="col">
-                        <span className="user-name lead fw-bold mx-3 chat-name">{message.userName}</span>
-                        <span className="review-sent fw-lighter">{message.createdAt}</span>
+                    <div className="row">
+                        <div className="col d-flex justify-content-between">
+                            <span className="user-name lead fw-bold mx-3 chat-name">{message.userName}</span>
+                            <span className="review-sent fw-lighter">{(message.createdAt).substring(0, 10)} 
+                            </span>
+                        </div>
                     </div>
-                </div>
-                <div className="review pt-2">{message.message}</div>
-            </li>
+                    <div className="review pt-2">{message.message}</div>
+                </li>
             )
         }))
-    } 
+    }
     else {
         return <FontAwesomeIcon icon={faSpinner} spin />
     }
